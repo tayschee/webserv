cc32029455c3828fe4abf1ad9d677fad65e130fc
tbigot <ta.tay@live.fr>
2021-03-17 11:43:21 +0100

    add private variable

--- include/server.hpp
+++ include/server.hpp
@@ -2,12 +2,20 @@
 # define SERVER_HPP
 
 /*Include below, put a comment next to it to say what function we use inside*/
+#include <string> //std::string
 
 class   server
 {
     public : /*typedef if neccessary*/
 
     private : /*private variables we can put a comment next to them to understand their utility*/
+
+        int			port;
+        int			ip;
+        std::string	domain_name; //to replace ip adress
+		int			socket_host; //to identify host
+		int			socket_client; //to identify client
+
 
     private : /*private function*/
 
