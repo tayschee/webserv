090b4d5506b6286679eec40a4974c52ac21aa272
Taylee Bigot <tatay@Taylees-MacBook-Air.local>
2021-03-17 10:48:31 +0100

    creation de la class server

---
+++ server.hpp
@@ -0,0 +1,19 @@
+#ifndef SERVER_HPP
+# define SERVER_HPP
+
+/*Ca serait bien de mettre a coté des includes les fonctions 
+qu'on utilise a l'interieurcomme ça on sait a quoi elles servent*/
+
+class   server
+{
+    public : /*typedef si necessaire*/
+
+    private : /*variable privée pourquoi pas mettre aussi un petit commentaire sur leurs utilités*/
+
+    private : /*fonction privée*/
+
+    public : /*fonction public*/
+
+};
+
+#endif
