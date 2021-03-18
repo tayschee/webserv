a34d5a9ad5e773b4d4cdcd3bee50d53926de2d9d
Ghali Boucetta <boucettaghali@gmail.com>
2021-03-18 12:07:32 +0100

    Basic coplien form (no processing except for ip string-uint32 encoding)

--- include/server.hpp
+++ include/server.hpp
@@ -11,15 +11,19 @@ class   server
     private : /*private variables we can put a comment next to them to understand their utility*/
 
         int			port;
-        int			ip;
+        uint32_t	ip;
         std::string	domain_name; //to replace ip adress
 		int			socket_host; //to identify host
 		int			socket_client; //to identify client
 
 
     private : /*private function*/
+		server(); // cannot declare a server without port/ip...
+		server(const server& other); // cannot copy server
+		server& operator=(const server& other); // cannot copy server
 
     public : /*public function*/
+		server(const std::string& domain, const std::string& ip, int port);	
 
 };
 
--- server/coplien.cpp
+++ server/coplien.cpp
@@ -3,3 +3,46 @@
 */
 
 #include "server.hpp"
+#include <iostream>
+
+#include <arpa/inet.h>
+#include <netinet/ip.h>
+
+// defining all functions used for canonique mode.
+server::server()
+{}
+
+server::server(const server& other)
+{
+	(void)other;
+}
+
+server &server::operator=(const server& other)
+{
+	(void)other;
+	return *this;
+}
+
+// defining the "normal" constructor used to create the server.
+// _ip is given as a string under ipv4 format.
+server::server(const std::string& _domain, const std::string& _ip, int _port) : domain_name(_domain), port(_port)
+{
+	std::string bytes[4];
+	int index = 0;
+
+	for (int i = 0; i < _ip.size(); i++)
+	{
+		if (::isdigit(_ip[i]))
+			bytes[index].append(1, _ip[i]);
+		else
+			index++;
+	}
+
+	ip = 0;
+	for (int i = 0; i < 4; i++)
+		ip |= std::stoi(bytes[i]) << (8 * i);
+}
+
+server::~server()
+{
+}
