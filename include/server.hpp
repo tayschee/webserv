7956a00e0493655c8c1d64c2bc99c57e8a0f3d48
Ghali Boucetta <boucettaghali@gmail.com>
2021-03-18 17:14:19 +0100

    First version of server (still getting ome errors on accepting)

--- include/server.hpp
+++ include/server.hpp
@@ -2,7 +2,17 @@
 # define SERVER_HPP
 
 /*Include below, put a comment next to it to say what function we use inside*/
+#include <sys/types.h>
 #include <string> //std::string
+#include <cstring> // strerror
+#include <errno.h> // errno
+#include <netinet/ip.h> // sockaddr_in
+#include <sys/socket.h> // socket, bind, setsockopt, accept
+#include <vector> // std::vector
+#include <sys/select.h> // select, fd_set, 
+#include <sys/time.h> // FD_ZERO, FD_SET, FD_ISSET
+#include <algorithm> // std::max, std::find
+#include <unistd.h> // close
 
 class   server
 {
@@ -10,12 +20,12 @@ class   server
 
     private : /*private variables we can put a comment next to them to understand their utility*/
 
-        int			port;
-        uint32_t	ip;
-        std::string	domain_name; //to replace ip adress
-		int			socket_host; //to identify host
-		int			socket_client; //to identify client
-
+        int					port;
+        uint32_t			ip;
+        std::string			domain_name; //to replace ip adress
+		int					socket_host; //to identify host
+		std::vector<int>	socket_client; //to identify clients
+		sockaddr_in			address;
 
     private : /*private function*/
 		server(); // cannot declare a server without port/ip...
@@ -24,7 +34,9 @@ class   server
 
     public : /*public function*/
 		server(const std::string& domain, const std::string& ip, int port);	
-
+		~server();
+		
+		void start(); // Calls listen and starts the server
 };
 
 #endif
