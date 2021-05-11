2bb2f0f7921221186c29f3ff2d99e7cdd6e45405
jelarose <jelarose@student.42.fr>
2021-05-11 13:41:59 +0200

    rectification des fichier apres le plantage de ma VM

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 
 int		main()
 {
-	cluster cl("/home/user42/42/web/conf/conf2");
+	cluster cl("/home/user42/42/webserv/conf/conf2");
 	cl.init_listen();
 	cl.start();
 	std::cout << "WEBSER C'EST FERMER CORRECTEMENT" << std::endl;
