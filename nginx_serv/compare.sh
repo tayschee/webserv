46e950c8fc498a6991226084ff7fd4b8604b8708
Taylee BIGOT <tbigot@e1r10p13.42.fr>
2021-07-07 14:59:33 +0200

    file to test

--- nginx_serv/compare.sh
+++ nginx_serv/compare.sh
@@ -1,4 +1,4 @@
 
-#docker run --rm -ti -v directory_containing_file:/config -v full_path_to_config_file:/etc/nginx/conf.d/default.conf -p 80:80 nginx_serv
+#docker run --rm -ti -v $(pwd)/directory:/config/ -v $(pwd)/conf:/etc/nginx/conf.d/default.conf -p 80:80 nginx_serv
 
 
