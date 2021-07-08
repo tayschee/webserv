b5c15105fb3eaf52a1e9db31ca62631c97106301
Taylee BIGOT <tbigot@e1r11p11.42.fr>
2021-07-08 15:23:41 +0200

    push

--- tester/my_test.sh
+++ tester/my_test.sh
@@ -54,7 +54,7 @@ curl -i -L -X GET 127.0.0.1:80/html/3.html
 
 docker stop $CONTAINER_NAME
 
-#Error on multiple test
+#MULTIPLE ERROR TEST
 docker run -d --rm -ti -v $NG_SRCS_PATH:$VM_SRCS_PATH -v $NG_SAME_ERROR_CONF:$VM_CONFIG_PATH -p 80:80 --name $CONTAINER_NAME $IMAGE_NAME
 sleep 5
 
@@ -64,7 +64,24 @@ curl -i -X GET 127.0.0.1:80/secret/secret.html
 curl -i -X UNKNOW_METHOD 127.0.0.1:80/html/3.html
 
 docker stop $CONTAINER_NAME
+
 C
+
+#MULTIPLE LOCATION TEST
+docker run -d --rm -ti -v $NG_SRCS_PATH:$VM_SRCS_PATH -v $NG_MULTIPLE_LOCATION_CONF:$VM_CONFIG_PATH -p 80:80 --name $CONTAINER_NAME $IMAGE_NAME
+sleep 5
+
+curl -i -X GET 127.0.0.1:80/gif/
+curl -i -X GET 127.0.0.1:80/html/
+curl -i -X GET 127.0.0.1:80/jpeg/
+curl -i -X GET 127.0.0.1:80/png/
+curl -i -X GET 127.0.0.1:80/private/
+curl -i -X GET 127.0.0.1:80/secret/
+curl -i -X GET 127.0.0.1:80/spoiler/
+curl -i -X GET 127.0.0.1:80/error/
+curl -i -X UNKNOW_METHOD 127.0.0.1:80/html/3.html
+
+docker stop $CONTAINER_NAME
 
 #permission reset
 chmod 755 srcs/private #because it s a directory
