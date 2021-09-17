6cea881edbbbdb9bfbac90765d8716a57fb259db
tbigot <tbigot@student.42.fr>
2021-09-17 17:36:14 +0200

    push

--- srcs/cluster/public.cpp
+++ srcs/cluster/public.cpp
@@ -22,41 +22,40 @@ int cluster::start() // cluster manage the list of socketc
 
 	while (true)
 	{
-		//reset timer
-		if (!wait_activity(readfds, writefds))
-			return 0;
+		try
+		{		
+			//reset timer
+			if (!wait_activity(readfds, writefds))
+				return 0;
 
-		iterator end = list_client.end();
-		for (iterator it = list_client.begin(); it != end; ++it)
-		{
-			int ret = 0;
-			client &cli = *(*it);
-
-			if (!cli.is_read() && FD_ISSET(cli.get_fd(), &readfds)) // is there a modification on the current list_client ?
+			iterator end = list_client.end();
+			for (iterator it = list_client.begin(); it != end; ++it)
 			{
-				if ((ret = receive(cli)) == 0)
-					return 0;
-				else if (ret == -1)
+				int ret = 0;
+				client &cli = *(*it);
+
+				if (!cli.is_read() && FD_ISSET(cli.get_fd(), &readfds)) // is there a modification on the current list_client ?
 				{
-					close_client(it);
+					if ((ret = receive(cli)) == 0)
+						return 0;
+					else if (ret == -1)
+					{
+						close_client(it);
+					}
 				}
-			}
-			else if (cli.is_read() && FD_ISSET(cli.get_fd(), &writefds))
-			{
-				if (cli.sent(vec_parser) == -1)
+				else if (cli.is_read() && FD_ISSET(cli.get_fd(), &writefds))
 				{
-					close_client(it);
+					if (cli.sent(vec_parser) == -1)
+					{
+						close_client(it);
+					}
 				}
 			}
 		}
-
-		/*for(iterator it = list_client.begin(); it != list_client.end(); it++)
+		catch(std::bad_alloc &e)
 		{
-			if (FD_ISSET(it->get_fd(), &readfds) || FD_ISSET(it->get_fd(), &writefds)) // is there a modification on the current list_client ?
-			{
-				//reset_timer
-			}
-		}*/
+			std::cerr << e.what();
+		}
 	}
 	return (0);
 }
